cc8c40a0d9d41624a5b2d3349bfa31b342fdd659
shaxobiddindev <shaxobiddindev@gmail.com>
2025-12-18 22:48:18 +0500

    sgadhasjgjh

---
+++ app.js
@@ -0,0 +1,28 @@
+import {showErrorModal, showSuccessModal} from './notificationModal.js';
+
+
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("navLinks");
+const navOverlay = document.getElementById("navOverlay");
+const navClose = document.getElementById("navClose");
+
+function openMenu() {
+    navClose.style.display = "block";
+    navLinks.classList.add("active-menu");
+    navOverlay.classList.add("active");
+    document.body.classList.add("menu-open");
+}
+
+function closeMenu() {
+    navLinks.classList.remove("active-menu");
+    navOverlay.classList.remove("active");
+    document.body.classList.remove("menu-open");
+}
+
+function showModal() {
+    showErrorModal("Kechirasiz bu hunksiya hali ishlab chiqlmagan!");
+}
+
+burger.addEventListener("click", showModal);
+navClose.addEventListener("click", closeMenu);
+navOverlay.addEventListener("click", closeMenu);
